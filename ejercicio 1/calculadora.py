28508488b106454f25a0960e063833ee55e23379
juanmejia02 <juanjillocandelo@gmail.com>
2024-01-20 11:56:21 -0500

    ejercicio 1 adelanto

--- ejercicio 1/calculadora.py
+++ ejercicio 1/calculadora.py
@@ -1,19 +1,22 @@
 def calculadora():
-    num1 = float("Ingrese el primer número: ")
-    num2 = float("Ingrese el segundo número: ")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
     operacion = input("Ingrese la operación (+, -, *, /): ")
 
     if operacion == '+':
-        resultado = num + num2
+        resultado = num1 + num2
     elif operacion == '-':
         resultado = num1 - num2
     elif operacion == '*':
         resultado = num1 * num2
     elif operacion == '/':
-        resultado = num / num2
+        resultado = num1 / num2
     else:
         resultado = "Operación no válida"
 
-    print("Resultado: "resultado)
+    print("Resultado", resultado)
 
-calculdora()
+calculadora()
+
+# primero que todo en la parte de arriba en la funcion float se estaba denominando para poner numeros y le estabamos poniendo un comentario entonces simplemente
+# se le agrega un input al float del num1 y num2
